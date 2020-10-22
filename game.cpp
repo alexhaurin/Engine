6ce111034c7c0551a8f06a5d54b809a630adefa5
Alex Haurin <alex.haurin@gmail.com>
2020-10-22 15:15:10 -0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,22 +1,28 @@
 #include "game.h"
 
-Game::Game() {
+Game::Game()
+{
+	window = std::make_shared<sf::RenderWindow>(sf::VideoMode(1200, 800), "Engine2");
 
-	auto window = std::make_shared<sf::Window>();
+	auto texture = std::make_shared<sf::Texture>();
+	if (!texture->loadFromFile("/Users/Alex Haurin/Downloads/HD_Hoopla_Fish.jpg")) {
+		std::cout << "Failed to load hoopla" << std::endl;
+	}
+	sf::Sprite hoopla;
+	hoopla.setTexture(*texture);
 
 	std::cout << "Created" << std::endl;
 }
 
-Game::~Game() {
-
+Game::~Game()
+{
 	std::cout << "Destroyed" << std::endl;
-
 }
 
-void Game::Run() {
-
+void Game::Run()
+{
 	auto targetTime = std::chrono::milliseconds(1000 / 60);
-	while (1) {
+	while (window->isOpen()) {
 		auto startTime = std::chrono::high_resolution_clock::now();
 		HandleEvents();
 		Update(1.0);
@@ -25,10 +31,11 @@ void Game::Run() {
 		auto deltaTime = startTime - endTime;
 
 
-		if (deltaTime < targetTime) {
-
+		if (deltaTime < targetTime)
+		{
 			auto remainingTime = targetTime - deltaTime - std::chrono::milliseconds(3);
-			if (remainingTime > std::chrono::milliseconds(0)) {
+			if (remainingTime > std::chrono::milliseconds(0))
+			{
 				std::this_thread::sleep_for(remainingTime);
 
 				while ((std::chrono::high_resolution_clock::now() - startTime) < targetTime) {}
@@ -37,19 +44,35 @@ void Game::Run() {
 
 		std::cout << std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now() - startTime).count() / 1000 << std::endl;
 
-
-
 	}
 }
 
-void Game::HandleEvents() {
+void Game::HandleEvents()
+{
+	sf::Event evnt;
+
+	while (window->pollEvent(evnt))
+	{
+		switch (evnt.type)
+		{
+		case sf::Event::Closed:
+			window->close();
+			break;
+		case sf::Event::Resized:
+			break;
+		}
+	}
+}
+
+void Game::Update(double dt)
+{
 
 }
 
-void Game::Update(double dt) {
-
-}
-
-void Game::Draw() {
+void Game::Draw()
+{
+	window->clear();
+	window->draw(hoopla);
+	window->display();
 
 }
