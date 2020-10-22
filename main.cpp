c50462ea506b5e61ebc758c14e5d4f0b37668d32
Alex Haurin <alex.haurin@gmail.com>
2020-10-22 15:15:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,9 @@
 #include <memory>
 #include "game.h"
 
-int main() {
+int main()
+{
 	auto game = std::make_shared<Game>();
-
-
-	/*sf::RenderWindow window(sf::VideoMode(1200, 800), "Engine2", sf::Style::Default);
-	sf::Event evnt;
-
-	while (window.isOpen()) {
-		while (window.pollEvent(evnt)) {
-
-			switch (evnt.type)
-			{
-			case sf::Event::Closed:
-				window.close();
-				break;
-			case sf::Event::Resized:
-				break;
-			}
-		}
-	} */
-
 
 	game->Run();
 }
