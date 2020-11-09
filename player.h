fdd11a4507abda83b85d09e1590a61635a47b00a
Alex Haurin <alex.haurin@gmail.com>
2020-11-08 20:57:58 -0500

    Update player.h

--- player.h
+++ player.h
@@ -12,22 +12,24 @@ public:
 	void Update();
 	void Draw(std::shared_ptr<sf::RenderWindow> window);
 
-	bool CheckCollisions(std::vector<std::shared_ptr<Enemy>> enemyList);
-	void Shoot();
+	void Move(sf::Vector2f direction);
+	bool CheckEnemyCollisions(std::vector<std::shared_ptr<Enemy>> enemyList);
+	std::shared_ptr<Bullet> Shoot();
+	sf::Vector2f Normalize(sf::Vector2f& vector);
 
 	sf::Vector2f GetPosition();
+	float GetSpeed() const;
 
-	std::vector<std::shared_ptr<Bullet>> bulletList;
+	sf::Vector2f direction;
 
-public:
-	sf::Sprite sprite;
+private:
 	sf::Texture texture;
+	sf::Sprite sprite;
 
 	float W;
 	float H;
-
 	sf::Vector2f position;
-	sf::Vector2f direction;
 	sf::Vector2f pastDirection;
+
 	float speed;
 };
