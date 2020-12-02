17d18a86a05c11cd89280235dcd737392c63d9f4
Alex Haurin <alex.haurin@gmail.com>
2020-12-02 09:58:01 -0500

    Update bullet.cpp

--- bullet.cpp
+++ bullet.cpp
@@ -1,49 +1,44 @@
 #include "bullet.h"
 
-Bullet::Bullet(float x, float y, sf::Vector2f d)
+Bullet::Bullet(sf::Vector2f in_position, sf::Vector2f in_dir)
 {
 	if (!texture.loadFromFile("Images/krabbyPatty.jpg")) {
 		std::cout << "Failed to load burger" << std::endl;
 	}
 	sprite.setTexture(texture);
 	sprite.setScale(sf::Vector2f(0.2f, 0.2f));
+	
+	position = in_position;
+	sprite.setPosition(position);
 
-	speed = 30.0f;
+	speed = 15.0f;
+	direction = in_dir;
 	W = texture.getSize().x * sprite.getScale().x;
 	H = texture.getSize().y * sprite.getScale().y;
-	sprite.setPosition(x - (W / 2), y - (H / 2));
-	position = GetPosition();
-	direction = d;
-
-	//std::cout << "Bullet Created" << std::endl;
 }
 
 Bullet::~Bullet()
 {
-	//std::cout << "Bullet Destroyed" << std::endl;
+
+}
+
+void Bullet::Initialize()
+{
+	Entity::Initialize();
+}
+
+void Bullet::Destroy()
+{
+	Entity::Destroy();
 }
 
 void Bullet::Update()
 {
-	position = GetPosition();
-	sprite.move(direction * 30.0f);
+	Entity::Update();
+	Move(direction);
 }
 
 void Bullet::Draw(std::shared_ptr<sf::RenderWindow> window)
 {
-	window->draw(sprite);
-}
-
-sf::Vector2f Bullet::GetPosition()
-{
-	sf::Vector2f p = sprite.getPosition();
-	p.x += W / 2;
-	p.y += H / 2;
-
-	return p;
-}
-
-sf::Vector2f Bullet::GetDimensions() const
-{
-	return sf::Vector2f(W, H);
+	Entity::Draw(window);
 }
